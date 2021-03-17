9cbaeca8e69c3e93c39f06332532f8489bf2381d
Matheus-mVilela <matheus_vilela@icloud.com>
2021-03-17 07:46:42 -0300

    create models and files for API - 2

--- pontos_turisticos/core/models.py
+++ pontos_turisticos/core/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from atracoes.models import Atracao
+from comentarios.models import Comentario
+from avaliacoes.models import Avaliacoes
 
 
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=150)
     descricao = models.TextField(max_length=300)
     aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+    comentarios = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacoes)
 
     def __str__(self):
         return self.nome
