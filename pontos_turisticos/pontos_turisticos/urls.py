5b343cfcf3d25901bb99eaea63b6b583b963f16e
Matheus-mVilela <matheus_vilela@icloud.com>
2021-03-17 21:49:11 -0300

    create all endpoints for API

--- pontos_turisticos/pontos_turisticos/urls.py
+++ pontos_turisticos/pontos_turisticos/urls.py
@@ -19,11 +19,16 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from avaliacoes.api.viewsets import AvaliacoesViewSet
+from comentarios.api.viewsets import ComentarioViewSet
+from enderecos.api.viewsets import EnderecoViewSet
+
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'avaliacoes', AvaliacoesViewSet)
+router.register(r'comentarios', ComentarioViewSet)
+router.register(r'endereco', EnderecoViewSet)
 
 
 urlpatterns = [
