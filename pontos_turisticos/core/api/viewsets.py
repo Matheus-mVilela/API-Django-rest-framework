d224d313fdefce132afad64b9d0f61c63d7af955
Matheus-mVilela <matheus_vilela@icloud.com>
2021-03-18 18:12:41 -0300

    filter - query string

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -6,8 +6,26 @@ from rest_framework.decorators import action
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    queryset = PontoTuristico.objects.all()
+    # queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+
+        queryset = PontoTuristico.objects.all()
+
+        if id:
+            queryset = PontoTuristico.objects.filter(pk=id)
+
+        if nome:
+            queryset = queryset.filter(nome__iexact=nome)
+
+        if descricao:
+            queryset = queryset.filter(descricao__iexact=descricao)
+
+        return queryset
 
     # def queryset(self):
     #     return PontoTuristico.objects.all()
