7357d6c4efd976957c22f9ec0f1b0486eaa9bbab
Matheus-mVilela <matheus_vilela@icloud.com>
2021-03-19 08:11:55 -0300

    search_fields

--- pontos_turisticos/core/api/viewsets.py
+++ pontos_turisticos/core/api/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 from rest_framework.response import Response
@@ -8,6 +9,8 @@ from rest_framework.decorators import action
 class PontoTuristicoViewSet(ModelViewSet):
     # queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['nome', 'descricao', 'enderecos__linha1']
 
     def get_queryset(self):
         id = self.request.query_params.get('id', None)
